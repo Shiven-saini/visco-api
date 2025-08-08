bfd93c3ce74adb8274568f3a00051fe8839bf645
Shiven Saini <shiven.career@proton.me>
2025-08-08 17:58:31 +0530

    Feat: added auth jwt file

---
+++ app/auth.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from . import models
+
+# JWT Configuration
+SECRET_KEY = "G4sZ7NnhIjzpLxunrCh14T2N50pwkMJiVMzyF6cVAl8="  # Generated using : openssl rand -base64 32
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_DAYS = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        return username
+    except JWTError:
+        return None
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        return False
+    if user.password != password:  # Plain text comparison, will change to hashing in production : TODO : Shiven Saini
+        return False
+    return user
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
