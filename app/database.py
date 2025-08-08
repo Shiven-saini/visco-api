c33cea43095b461ea8d8790864c1149f91c01d3b
Shiven Saini <shiven.career@proton.me>
2025-08-08 17:55:07 +0530

    Feat: added postgresql credentials and connector
    Credentials :
    user => shiven
    password => Shiven@123

---
+++ app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Database configuration
+DATABASE_URL = "postgresql://shiven:Shiven@123@localhost:5432/visco"
+
+# Create SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create SessionLocal class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create Base class
+Base = declarative_base()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
